7c537b1bbec086e25d1367c4abcf25cb47665363
Kanayo <anayoifediora@live.com>
2025-12-22 11:34:47 +1030

    required dotenv

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
 //Import required Mongoose package
 const mongoose = require("mongoose");
-
+require('dotenv').config();
+console.log(process.env.MONGODB_URI)
 //Connects the app to the database using the Mongoose ODM library
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/quickBasket"
